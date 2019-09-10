232edecde5c72a9d34a8af01ba6b6f888d89b542
David Bailey <davidbailey00@outlook.com>
2019-09-11 00:00:21 +0100

    continue work on build process

--- packages/ninetales-build/src/index.js
+++ packages/ninetales-build/src/index.js
@@ -1,13 +1,25 @@
-import * as babel from "@babel/core";
+import { transformFileAsync } from "@babel/core";
 import recursive from "recursive-readdir";
+import { outputFile, copyFile } from "fs-extra";
 
 export default async function main() {
-  for (const file of await recursive("src")) {
-    const { code } = await babel.transformAsync(file, {
-      presets: ["@ninetales/build"],
-    });
+  // TODO: move these to a global user-modifiable config
+  const sourceDir = "src";
+  const outputDir = "dist";
+  const extensions = ["js", "jsx"];
 
-    console.log(code);
+  for (const file of await recursive(sourceDir)) {
+    const outFile = outputDir + file.slice(sourceDir.length);
+
+    if (extensions.find(ext => file.endsWith(`.${ext}`))) {
+      const { code } = await transformFileAsync(file, {
+        presets: ["@ninetales/build"],
+      });
+
+      await outputFile(outFile, code);
+    } else {
+      await copyFile(file, outFile);
+    }
   }
 }
 
