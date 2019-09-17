09d5461c4e8d33c5c68d66ca5857d53da61c647e
David Bailey <davidbailey00@outlook.com>
2019-09-17 20:09:17 +0100

    perform hydration!!

--- packages/ninetales/src/index.js
+++ packages/ninetales/src/index.js
@@ -1,8 +1,10 @@
+import { h, hydrate as preactHydrate } from "preact";
+
 export function notUsed() {
   console.log("test123 this should not appear in the bundle");
 }
 
 export function hydrate(View) {
-  // TODO: hydrate
-  console.log("starting ninetales runtime", View);
+  const props = JSON.parse(document.getElementById("props").innerHTML);
+  preactHydrate(<View {...props} />, document.getElementById("app"));
 }
