d8f5e2dbd6a6d47f04382448b5adb38192a273d9
David Bailey <davidbailey00@outlook.com>
2019-09-22 16:39:22 +0100

    Create navigate function

--- packages/ninetales/src/index.js
+++ packages/ninetales/src/index.js
@@ -1,10 +1,12 @@
-import { h, hydrate as preactHydrate } from "preact";
+import { h, hydrate } from "preact";
+import navigate from "./navigate";
 
 export function notUsed() {
   console.log("test123 this should not appear in the bundle");
 }
 
-export function hydrate(View) {
+export function startRuntime(View) {
   const props = JSON.parse(document.getElementById("props").innerHTML);
-  preactHydrate(<View {...props} />, document.getElementById("app"));
+  hydrate(<View {...props} />, document.getElementById("app"));
+  window.navigate = navigate; // TODO: remove later
 }
---
+++ packages/ninetales/src/navigate.js
@@ -0,0 +1,48 @@
+import { render } from "preact";
+
+// TODO: handle 404s
+async function transition({ html, data, bundles }) {
+  // fetch bundles before destroying current page
+  const bundleScripts = await Promise.all(
+    bundles.map(async bundle => {
+      const src = `/.bundles/${bundle}`;
+      const response = await fetch(src);
+      return await response.text();
+    })
+  );
+
+  // destroy existing root node
+  render(null, document.getElementById("app"));
+
+  const domparser = new DOMParser();
+  const newDocument = domparser.parseFromString(html, "text/html");
+  document.documentElement.replaceChild(newDocument.head, document.head);
+  document.documentElement.replaceChild(newDocument.body, document.body);
+
+  // TODO: handle status code?
+  const propsScript = document.createElement("script");
+  propsScript.id = "props";
+  propsScript.type = "application/json";
+  propsScript.innerHTML = JSON.stringify(data.props);
+  document.body.appendChild(propsScript);
+
+  bundleScripts.forEach(text => {
+    const bundleScript = document.createElement("script");
+    bundleScript.innerHTML = text;
+    document.body.appendChild(bundleScript);
+  });
+}
+
+// TODO: support POST
+async function navigate(href) {
+  const { pathname } = new URL(href, location.origin);
+  const dataURL = `/.data${pathname}`;
+
+  const response = await fetch(dataURL);
+  const data = await response.json();
+
+  await transition(data);
+  history.pushState(href, "", href);
+}
+
+export default navigate;
