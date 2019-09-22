a36cef525f6585798b35f7ceb950eb930cc62be0
David Bailey <davidbailey00@outlook.com>
2019-09-22 14:18:20 +0100

    fix aliased import stuff again

--- packages/ninetales-server/src/render-response.js
+++ packages/ninetales-server/src/render-response.js
@@ -1,13 +1,14 @@
 import config from "./config.json";
-import moduleAlias from "module-alias";
-moduleAlias.addAliases(config.alias);
+import HTML, { doctype } from "./html";
 
 import { h } from "preact";
 import render from "preact-render-to-string";
-const flush = require("styled-jsx/server").default;
-
-import HTML, { doctype } from "./html";
 import pretty from "pretty";
+import moduleAlias from "module-alias";
+
+// see: https://github.com/babel/babel/issues/2061
+moduleAlias.addAliases(config.alias);
+const flush = require("styled-jsx/server").default;
 
 function renderResponse(res, { View, bundles, full, data }) {
   if (data.status !== undefined) {
