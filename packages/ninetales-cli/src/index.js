b2572945481e92345b395bb4cc0bda760ad843f0
David Bailey <davidbailey00@outlook.com>
2019-09-23 20:09:36 +0100

    refactor cli

--- packages/ninetales-cli/src/index.js
+++ packages/ninetales-cli/src/index.js
@@ -3,30 +3,22 @@ import yargs from "yargs";
 
 // TODO: only catch require errors, not everything else
 
-yargs
-  .command(
-    "build",
-    "build a project",
-    () => {},
-    () => {
-      try {
-        require("@ninetales/build").default();
-      } catch (e) {
-        console.error("Error: command 'build' must be used inside a project");
-        process.exit(1);
-      }
-    }
-  )
-  .command(
-    "serve",
-    "serve a built project",
-    () => {},
-    () => {
-      try {
-        require("@ninetales/server").default();
-      } catch (e) {
-        console.log("Error: command 'serve' must be used inside a project");
-        process.exit(1);
-      }
-    }
-  ).argv;
+yargs.command("build", "build a project", () => {}, function() {
+  try {
+    require("@ninetales/build").default();
+  } catch (e) {
+    console.error("Error: command 'build' must be used inside a project");
+    process.exit(1);
+  }
+});
+
+yargs.command("serve", "serve a built project", () => {}, function() {
+  try {
+    require("@ninetales/server").default();
+  } catch (e) {
+    console.log("Error: command 'serve' must be used inside a project");
+    process.exit(1);
+  }
+});
+
+yargs.argv;
