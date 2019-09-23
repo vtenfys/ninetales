d6453746727ff0b867696ddaa4a3a2ce17bf2543
David Bailey <davidbailey00@outlook.com>
2019-09-23 23:03:49 +0100

    transform typeof window in browser bundles

--- packages/ninetales-babel-preset/build/browser.js
+++ packages/ninetales-babel-preset/build/browser.js
@@ -1,4 +1,13 @@
 module.exports = () => ({
   presets: [require("../browser")],
-  plugins: [require("babel-plugin-root-import"), require("styled-jsx/babel")],
+  plugins: [
+    require("babel-plugin-root-import"),
+    require("styled-jsx/babel"),
+    [
+      require("babel-plugin-transform-define"),
+      {
+        "typeof window": "object",
+      },
+    ],
+  ],
 });
