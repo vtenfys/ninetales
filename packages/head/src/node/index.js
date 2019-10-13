6afb93819aa0a15f996472b42f339f7083ac0210
David Bailey <davidbailey00@outlook.com>
2019-10-13 19:12:23 +0100

    comment reason for reverse()

--- packages/head/src/node/index.js
+++ packages/head/src/node/index.js
@@ -16,5 +16,8 @@ export default function Head({ children }) {
 }
 
 export function flush() {
+  // Children of new portals are mounted at the top of their respective
+  // container element, i.e. above children of previous portals, so match this
+  // order by reversing after flush.
   return headTags.splice(0, headTags.length).reverse();
 }
