a34179976e98c25f63c5431090dbfaa7f34b1c00
David Bailey <davidbailey00@outlook.com>
2019-10-26 16:30:38 +0100

    Add new package entrapta + remove unnecessary git add script

---
+++ packages/entrapta/src/index.js
@@ -0,0 +1,34 @@
+function createHandler(mutable, constructed) {
+  return {
+    get(target, prop) {
+      if (!Object.prototype.hasOwnProperty.call(constructed, prop)) {
+        const value = Reflect.get(...arguments);
+
+        if (typeof value === "object") {
+          // recursively observe objects
+          constructed[prop] = Array.isArray(value) ? [] : {};
+          mutable[prop] = createObserver(value, constructed[prop]);
+        } else {
+          constructed[prop] = value;
+        }
+      }
+
+      return mutable[prop];
+    },
+  };
+}
+
+function createObserver(target, constructed) {
+  // clone target to prevent directly modifying it
+  const mutable = Array.isArray(target) ? [...target] : { ...target };
+  const handler = createHandler(mutable, constructed);
+
+  return new Proxy(target, handler);
+}
+
+export default function observe(obj) {
+  const constructed = {};
+  const observable = createObserver(obj, constructed);
+
+  return [observable, constructed];
+}
