673324d0a65560d73adf34f61072b1c473a72565
David Bailey <davidbailey00@outlook.com>
2019-10-27 15:32:18 +0000

    rephrase props observation comment

--- packages/server/src/render-response.js
+++ packages/server/src/render-response.js
@@ -10,10 +10,10 @@ function renderResponse(res, { View, assets, data }) {
     res.status(data.status);
   }
 
-  // Preact spreads props when creating a VNode, which counts as an observation
-  // of all properties. So initially, create a VNode using the original props,
-  // then observe the spreaded props rather than the original ones, so that we
-  // can strip top-level props which are unused entirely.
+  // Preact spreads props when creating a VNode, which is treated as accessing
+  // all properties of the props object. So initially, create a VNode using the
+  // original props, then observe the spreaded props rather than the original
+  // ones, so that we can strip top-level props which are unused entirely.
   const view = <View {...data.props} />;
 
   // only props accessed via observableProps make it to constructedProps
