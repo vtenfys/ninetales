2afe9882ebc2082894a03176f51d1f1ab2a6eacd
David Bailey <davidbailey00@outlook.com>
2019-10-27 11:52:07 +0000

    Implement stripping unused props

--- packages/server/src/render-response.js
+++ packages/server/src/render-response.js
@@ -3,17 +3,22 @@ import HTML, { doctype } from "./html";
 import { h } from "preact";
 import render from "preact-render-to-string";
 import { flush } from "@ninetales/head";
+import observe from "@ninetales/entrapta";
 
 function renderResponse(res, { View, assets, data }) {
   if (data.status !== undefined) {
     res.status(data.status);
   }
 
+  // deep strip unused props:
+  // only props accessed via observableProps make it to constructedProps
+  const [observableProps, constructedProps] = observe(data.props);
+
   const htmlProps = {
-    app: render(<View {...data.props} />),
+    app: render(<View {...observableProps} />),
     lang: data.lang,
     head: flush(),
-    props: data.props,
+    props: constructedProps,
     assets,
   };
 
